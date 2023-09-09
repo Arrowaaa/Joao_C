a04abc8ed17bd88a090ed3207e1324708aea29e4
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2023-09-09 19:37:49 -0300

    Update Program.cs
    Melhorias

--- correcao/Program.cs
+++ correcao/Program.cs
@@ -33,45 +33,50 @@ namespace Loja
 
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.White; // esta linha de código define a cor de fundo do console como branco 
+            Console.ForegroundColor = ConsoleColor.DarkBlue; //esta linha de código define a cor do texto no console para azul escuro
             Console.WriteLine("\r\n ____  _  ____  _____  _____ _      ____    ____  ____    ____  _____ _          _  ____  ____  ____ \r\n/ ___\\/ \\/ ___\\/__ __\\/  __// \\__/|/  _ \\  /  _ \\/  _ \\  / ___\\/  __// \\ /\\     / |/  _ \\/  _ \\/  _ \\\r\n|    \\| ||    \\  / \\  |  \\  | |\\/||| / \\|  | | \\|| / \\|  |    \\|  \\  | | ||     | || / \\|| / \\|| / \\|\r\n\\___ || |\\___ |  | |  |  /_ | |  ||| |-||  | |_/|| \\_/|  \\___ ||  /_ | \\_/|  /\\_| || \\_/|| |-||| \\_/|\r\n\\____/\\_/\\____/  \\_/  \\____\\\\_/  \\|\\_/ \\|  \\____/\\____/  \\____/\\____\\\\____/  \\____/\\____/\\_/ \\|\\____/\r\n                                                                                                     \r\n");
             
             bool continuarCadastrando = true;
 
-    while (continuarCadastrando)
+    while (continuarCadastrando) // loop
     {
         MostrarMenu();
         int escolha = int.Parse(Console.ReadLine());
 
-        switch (escolha)
+        switch (escolha) //estrutura de controle chamada switch, que é usada para tomar decisões com base no valor de escolha.
         {
             case 1:
             
                 CadastrarProduto();
                 continuarCadastrando = PerguntarContinuarCadastrando();
                 break;
+                
             case 2:
             
                 VenderProduto();
                 continuarCadastrando = PerguntarContinuarCadastrando();
                 break;
+                
             case 3:
             
                 ComprarProduto();
                 continuarCadastrando = PerguntarContinuarCadastrando();
                 break;
+                
             case 4:
             
                 GerarRelatorio();
                 continuarCadastrando = PerguntarContinuarCadastrando();
                 break;
+                
             case 5:
             
                 Console.Clear();
                 continuarCadastrando = false; // Sair do loop e encerrar o programa
                 break;
-            default:
+                
+            default: //para lidar com escolhas que não correspondem a nenhum dos casos específicos.
             
                 Console.WriteLine("Opção inválida. Tente novamente.");
                 break;
@@ -104,6 +109,13 @@ namespace Loja
     }
 
         static void MostrarMenu()
+        //static void: é um método estático que não retorna um valor e não aceita parâmetros.
+        //Geralmente, esse tipo de método é usado para realizar uma ação específica, 
+        //como exibir um menu na tela para interação do usuário, como escolher opções em um programa.
+        //indica que o método MostrarMenu não retorna nenhum valor quando é executado.
+        //Isso significa que o método não produz um resultado que seja retornado quando é chamado.
+        //Em vez disso, ele pode executar ações, mas não retorna um valor.
+        //MostrarMenu()é usado para identificar e chamar o método quando necessário.
     
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -137,7 +149,11 @@ namespace Loja
             int quantidade = int.Parse(Console.ReadLine());
 
             Produto novoProduto = new Produto(nome, marca, preco, quantidade);
-            produtos.Add(novoProduto);
+            produtos.Add(novoProduto); 
+            //Esta linha cria uma nova instância da classe Produto e a atribui à variável novoProduto.
+            //O construtor da classe Produto é chamado neste momento e recebe quatro argumentos.
+            //O método .Add é um método que pertence à lista (ou coleção) e é usado para inserir um elemento nela.
+            //gerenciam inventários, catálogos ou qualquer situação em que você precise manter uma coleção de objetos para posterior acesso e manipulação.
 
             Console.WriteLine("\n Produto cadastrado com sucesso!");
             
@@ -157,6 +173,9 @@ namespace Loja
             int escolha = int.Parse(Console.ReadLine()) - 1;
 
             if (escolha >= 0 && escolha < produtos.Count)
+            
+            //produtos: é uma lista (ou uma coleção) que contém elementos,
+            //Count é uma propriedade que retorna o número de elementos atualmente na lista.
         {
             Produto produto = produtos[escolha];
             Console.Write("\n Quantidade de " + produto.Nome + " a ser vendida: ");
@@ -284,8 +303,15 @@ namespace Loja
                 Console.WriteLine("\n Histórico de Vendas:");
 
                 foreach (var venda in vendas)
+                // Código para processar cada elemento da coleção vendas,
+                // usando a variável venda para acessar o elemento atual.
+                // Essa é a palavra-chave que inicia o loop foreach. 
+                //Ela indica que você deseja executar um bloco de código para cada elemento em uma coleção 
+                //(neste caso, a coleção é representada por vendas).
             {
                 Console.WriteLine("Produto: " + venda.Produto + ", Valor: " + venda.ValorTotal.ToString("F2") + " Reais, Forma de Pagamento: " + venda.FormaPagamento + ", Parcelas: " + venda.NumeroParcelas);
+            //.ToString("F2"): é uma formatação que converte o valor em uma string formatada com duas casas decimais (formato de ponto flutuante com precisão de duas casas). 
+            //Isso garante que o valor seja exibido com duas casas decimais, por exemplo, "50.00" em vez de "50".
             }
         } 
 
@@ -295,7 +321,10 @@ namespace Loja
             for (int i = 0; i < produtos.Count; i++)
             {
                 Console.WriteLine((i + 1) + ". " + produtos[i].Nome + " - Marca: " + produtos[i].Marca + " - Preço: R$ " + produtos[i].Preco.ToString("F2") + " - Estoque: " + produtos[i].Quantidade);
-
+            //(i + 1): i é uma variável de loop. Geralmente, i é usada como um índice para percorrer a lista produtos.
+            //A expressão (i + 1) está sendo usada para mostrar o número da posição do produto na lista. 'i' ajusta essa posição para começar em 1.
+            //Portanto, esta linha está formatando e exibindo informações sobre um produto, incluindo sua posição na lista, nome, marca e preço em reais. É uma maneira comum de listar produtos ou itens em um programa, onde cada linha representa um produto específico com detalhes formatados. 
+            
             }
         }
     }
@@ -305,6 +334,11 @@ namespace Loja
         public double ValorTotal { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
         public int NumeroParcelas { get; set; }
+        
+        //Piblic: é um modificador de acesso que indica a visibilidade de classes, métodos, propriedades e outros membros dentro de um programa.
+        //ele pode ser acessado e usado de fora da classe, ou seja, de outros lugares do programa, incluindo outras classes e partes do código.
+        //No contexto de uma propriedade, get é usado para obter o valor da propriedade,
+        //e set é usado para definir o valor da propriedade
     }
 
 
@@ -317,6 +351,9 @@ namespace Loja
         public int Quantidade { get; set; }
 
         private static int proximoNumeroSerie = 1;
+        //Private: só pode ser acessada dentro da classe em que foi declarada. 
+        //Ela não pode ser acessada diretamente de fora da classe.
+        //= 1: Isso é uma inicialização da variável. Ele define o valor inicial da variável proximoNumeroSerie como 1.
 
         public Produto(string nome, string marca, double preco, int quantidade)
         {
